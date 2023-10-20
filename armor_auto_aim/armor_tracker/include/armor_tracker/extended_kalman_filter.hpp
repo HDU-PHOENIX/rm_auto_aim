6c2ab0f7ea9e3159cde919b199617cf49cd6b5a5
Bob0817912 <2099572595@qq.com>
2023-10-20 19:42:43 +0800

    更改了tracker的部分内容

---
+++ armor_auto_aim/armor_tracker/include/armor_tracker/extended_kalman_filter.hpp
@@ -0,0 +1,75 @@
+// Copyright 2023 wangchi
+#ifndef ARMOR_PROCESSOR__KALMAN_FILTER_HPP_
+#define ARMOR_PROCESSOR__KALMAN_FILTER_HPP_
+
+#include <Eigen/Dense>
+#include <functional>
+
+namespace rm_auto_aim
+{
+// 扩展卡尔曼滤波器类
+class ExtendedKalmanFilter
+{
+public:
+  ExtendedKalmanFilter() = default;
+
+  // 函数类型定义
+  using VecVecFunc = std::function<Eigen::VectorXd(const Eigen::VectorXd &)>;
+  using VecMatFunc = std::function<Eigen::MatrixXd(const Eigen::VectorXd &)>;
+  using VoidMatFunc = std::function<Eigen::MatrixXd()>;
+
+  // 构造函数
+  explicit ExtendedKalmanFilter(
+    const VecVecFunc & f, const VecVecFunc & h, const VecMatFunc & j_f, const VecMatFunc & j_h,
+    const VoidMatFunc & u_q, const VecMatFunc & u_r, const Eigen::MatrixXd & P0);
+
+  // 设置初始状态
+  void setState(const Eigen::VectorXd & x0);
+
+  // 预测一个时间步
+  Eigen::MatrixXd predict();
+
+  // 根据测量更新估计状态
+  Eigen::MatrixXd update(const Eigen::VectorXd & z);
+
+private:
+  // 处理非线性向量函数
+  VecVecFunc f;
+  // 观测非线性向量函数
+  VecVecFunc h;
+  // f()的雅可比矩阵
+  VecMatFunc jacobian_f;
+  Eigen::MatrixXd F;
+  // h()的雅可比矩阵
+  VecMatFunc jacobian_h;
+  Eigen::MatrixXd H;
+  // 过程噪声协方差矩阵
+  VoidMatFunc update_Q;
+  Eigen::MatrixXd Q;
+  // 测量噪声协方差矩阵
+  VecMatFunc update_R;
+  Eigen::MatrixXd R;
+
+  // 先验误差估计协方差矩阵
+  Eigen::MatrixXd P_pri;
+  // 后验误差估计协方差矩阵
+  Eigen::MatrixXd P_post;
+
+  // 卡尔曼增益
+  Eigen::MatrixXd K;
+
+  // 系统维度
+  int n;
+
+  // N维单位矩阵
+  Eigen::MatrixXd I;
+
+  // 先验状态
+  Eigen::VectorXd x_pri;
+  // 后验状态
+  Eigen::VectorXd x_post;
+};
+
+}  // namespace rm_auto_aim
+
+#endif  // ARMOR_PROCESSOR__KALMAN_FILTER_HPP_
---
+++ armor_auto_aim/armor_tracker/include/armor_tracker/tracker_node.hpp
@@ -0,0 +1,83 @@
+// Copyright 2023 wangchi
+#ifndef ARMOR_PROCESSOR__PROCESSOR_NODE_HPP_
+#define ARMOR_PROCESSOR__PROCESSOR_NODE_HPP_
+
+// ROS
+#include <message_filters/subscriber.h>
+#include <tf2_ros/buffer.h>
+#include <tf2_ros/create_timer_ros.h>
+#include <tf2_ros/message_filter.h>
+#include <tf2_ros/transform_listener.h>
+
+#include <rclcpp/rclcpp.hpp>
+#include <std_srvs/srv/trigger.hpp>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+#include <visualization_msgs/msg/marker_array.hpp>
+
+// STD
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "armor_tracker/tracker.hpp"
+#include "auto_aim_interfaces/msg/armors.hpp"
+#include "auto_aim_interfaces/msg/target.hpp"
+#include "auto_aim_interfaces/msg/tracker_info.hpp"
+
+namespace rm_auto_aim
+{
+// 使用tf2_ros::MessageFilter对自动瞄准接口的Armors消息进行过滤
+using tf2_filter = tf2_ros::MessageFilter<auto_aim_interfaces::msg::Armors>;
+
+class ArmorTrackerNode : public rclcpp::Node
+{
+public:
+  explicit ArmorTrackerNode(const rclcpp::NodeOptions & options);
+
+private:
+  // 处理Armors消息的回调函数
+  void armorsCallback(const auto_aim_interfaces::msg::Armors::SharedPtr armors_ptr);
+
+  // 发布标记点函数
+  void publishMarkers(const auto_aim_interfaces::msg::Target & target_msg);
+
+  // XOY平面中允许的最大装甲距离
+  double max_armor_distance_;
+
+  // 上次接收消息的时间
+  rclcpp::Time last_time_;
+  double dt_;
+
+  // 装甲追踪器
+  double s2qxyz_, s2qyaw_, s2qr_;
+  double r_xyz_factor, r_yaw;
+  double lost_time_thres_;
+  std::unique_ptr<Tracker> tracker_;
+
+  // 重置追踪器服务
+  rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr reset_tracker_srv_;
+
+  // 使用tf2消息过滤器的订阅器
+  std::string target_frame_;
+  std::shared_ptr<tf2_ros::Buffer> tf2_buffer_;
+  std::shared_ptr<tf2_ros::TransformListener> tf2_listener_;
+  message_filters::Subscriber<auto_aim_interfaces::msg::Armors> armors_sub_;
+  std::shared_ptr<tf2_filter> tf2_filter_;
+
+  // 追踪器信息发布器
+  rclcpp::Publisher<auto_aim_interfaces::msg::TrackerInfo>::SharedPtr info_pub_;
+
+  // 发布器
+  rclcpp::Publisher<auto_aim_interfaces::msg::Target>::SharedPtr target_pub_;
+
+  // 可视化标记发布器
+  visualization_msgs::msg::Marker position_marker_;
+  visualization_msgs::msg::Marker linear_v_marker_;
+  visualization_msgs::msg::Marker angular_v_marker_;
+  visualization_msgs::msg::Marker armor_marker_;
+  rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr marker_pub_;
+};
+
+}  // namespace rm_auto_aim
+
+#endif  // ARMOR_PROCESSOR__PROCESSOR_NODE_HPP_
