de9b9565e6508538d5081a4df787d96d5f207d9a
PTttt123 <2534732180@qq.com>
2023-10-25 20:30:39 +0800

    暂时增加shooter模块 解算弹道

---
+++ shooter/rune_shooter/include/rune_shooter/rune_shooter.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <rclcpp/rclcpp.hpp>
+
+namespace rune_shooter 
+{
+    class RuneShooterNode : public rclcpp::Node
+    {
+    public:
+        explicit RuneShooterNode(const rclcpp::NodeOptions& options);
+
+    private:
+
+    };
+};
