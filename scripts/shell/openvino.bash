e0e04881f41a6251018e6789ae8fef64a1746485
jalen <jalenchuh@gmail.com>
2023-11-17 17:54:26 +0800

    :bug: 更新脚本

--- scripts/shell/openvino.bash
+++ scripts/shell/openvino.bash
@@ -1,9 +1,9 @@
 #!bash
 echo "start install curl"
-sudo apt install curl
+sudo apt update && sudo apt install curl -y
 echo "successfully install curl"
 curl https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB > /tmp/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
-if [-f /tmp/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB ]; then
+if [ -f /tmp/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB ]; then
     echo "successfully wget  GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB"
 else
     echo "wget GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB failed"
@@ -14,9 +14,5 @@ sudo apt-key add /tmp/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
 
 echo "deb https://apt.repos.intel.com/openvino/2023 ubuntu22 main" | sudo tee /etc/apt/sources.list.d/intel-openvino-2023.list # ubuntu22
 
-sudo apt update
-
-apt-cache search openvino
-
-sudo apt install openvino
+sudo apt update && sudo apt install openvino -y
 echo "successfully install openvino"
