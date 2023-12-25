61463a7d07a9383ae3f09e6d73d542084d96d5ae
jalen <jalenchuh@gmail.com>
2023-12-25 19:27:43 +0800

    :rocket: 完善环境安装脚本

--- scripts/setup.bash
+++ scripts/setup.bash
@@ -1,12 +1,27 @@
 #!bash
 
+echo ">>> APT update" && sleep 1
 sudo apt update
-echo ">>> start install wget"
-sudo apt install wget -y
-echo ">>> successfully install wget\n"
 
-echo "\n>>> start install openvino\n"
-sleep 1
+echo "\n\n>>> wget ceres pip" && sleep 1
+sudo apt install wget libceres-dev python3-pip -y
+
+echo "\n\n>>> MindVision SDK" && sleep 1
+sudo apt install wget -y
+if [ -d /usr/include/mindvision/ ]; then
+    echo "mindvision-sdk already installed"
+else
+    echo ">>> start install mindvision-sdk"
+    mkdir mindvision-sdk
+    wget https://www.mindvision.com.cn/uploadfiles/SDK/linuxSDK_V2.1.0.37.tar.gz -O mindvision-sdk/sdk.tar.gz
+    tar -zxvf mindvision-sdk/sdk.tar.gz --directory=mindvision-sdk
+    sed -i 's/usr\/include/usr\/include\/mindvision/g' mindvision-sdk/install.sh
+    sed -i 17a\\"mkdir -p /usr/include/mindvision" mindvision-sdk/install.sh
+    cd mindvision-sdk && sudo bash ./install.sh && cd ..
+    echo ">>> successfully install mindvision-sdk"
+fi
+
+echo "\n\n>>> OpenVINO" && sleep 1
 wget https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB -O /tmp/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
 if [ -f /tmp/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB ]; then
     echo "successfully wget GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB"
@@ -14,19 +29,11 @@ else
     echo "wget GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB failed"
     exit -1
 fi
-
 sudo apt-key add /tmp/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB 
 echo "deb https://apt.repos.intel.com/openvino/2023 ubuntu22 main" | sudo tee /etc/apt/sources.list.d/intel-openvino-2023.list # ubuntu22
 sudo apt update && sudo apt install openvino -y
-echo ">>> successfully install openvino"
 
-echo "\n>>> start install ceres\n"
-sleep 1
-sudo apt install libceres-dev -y
-echo ">>> successfully install ceres"
-
-echo "\n>>> start install ros2\n"
-sleep 1
+echo "\n\n>>> ROS 2" && sleep 1
 if ! command -v ros2 &> /dev/null
 then
     echo "ROS is not installed, start install ROS2"
@@ -35,12 +42,12 @@ else
     echo "ROS installed, skip install ROS2"
 fi
 
-echo "\n>>> start install ros2 dependencies\n"
-sleep 1
-if [ -f /etc/ros/rosdep/sources.list.d ]; then
+echo "\n\n>>> rosdep" && sleep 1
+if [ -d /etc/ros/rosdep/sources.list.d ]; then
     echo "rosdep already init"
 else
     sudo rosdep init
+    sudo pip3 install rosdepc
+    sudo rosdepc init
 fi
-rosdep update
-rosdep install --from-paths ./ --ignore-src -r -y
+sudo rosdep update && rosdep install --from-paths ./ --ignore-src -r -y
