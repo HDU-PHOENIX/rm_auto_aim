97381a736149d90e4d550bfbe397021ff31105db
Jalen <jalenchuh@gmail.com>
2024-04-21 21:03:12 +0800

    更新 mindvision sdk 链接

--- scripts/setup.bash
+++ scripts/setup.bash
@@ -13,8 +13,10 @@ if [ -d /usr/include/mindvision/ ]; then
 else
     echo ">>> start install mindvision-sdk"
     mkdir mindvision-sdk
-    wget http://10.2.20.150/file/SDK.zip -O mindvision-sdk/SDK.zip
-    tar -zxvf mindvision-sdk/SDK.zip --directory=mindvision-sdk
+    wget https://www.mindvision.com.cn/wp-content/uploads/2023/08/linuxSDK_V2.1.0.37.tar.gz -O mindvision-sdk/SDK.tar.gz
+    tar -zxvf mindvision-sdk/SDK.tar.gz --directory=mindvision-sdk
+    sed -i 's/usr\/include/usr\/include\/mindvision/g' mindvision-sdk/install.sh
+    sed -i 17a\\"mkdir -p /usr/include/mindvision" mindvision-sdk/install.sh
     cd mindvision-sdk && sudo bash ./install.sh && cd ..
     echo ">>> successfully install mindvision-sdk"
     rm -r mindvision-sdk
